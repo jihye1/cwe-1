7435601fbeba007a3f8462afb7f60afd135b3728
jihye1 <jihye.won1@samsung.com>
2026-04-28 17:21:38 +0900

    Update cwe.js

--- cwe.js
+++ cwe.js
@@ -10,3 +10,8 @@ function CWE_476() { // NULL_POINTER
     var y = obj.x;  // UNINITIALIZED_LOCAL_VAR
     console.log(y);
 }
+
+function CWE_480_481() { // BAD_ASSIGN_IN_CONDITION
+  let x = -1;
+  if (x = -1) console.log('Error!', x);
+}
